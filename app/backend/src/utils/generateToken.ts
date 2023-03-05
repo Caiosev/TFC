573b7a25351588ad0f6c8a794a722d4b8082d801
Caio Severino <kaiosev9@gmail.com>
2023-03-05 17:19:30 -0300

    bug:: 🐛 change jwt params

--- app/backend/src/utils/generateToken.ts
+++ app/backend/src/utils/generateToken.ts
@@ -1,10 +1,11 @@
 import jwt = require('jsonwebtoken');
 
-const key = process.env.JWT_SECRET || 'segredo';
+const key = process.env.JWT_SECRET || 'jwt_secret';
 
 export default function generateToken(email: string) {
   const token = jwt.sign({ data: { email } }, key, {
-    expiresIn: 86400,
+    expiresIn: '7d',
+    algorithm: 'HS256',
   });
   return token;
 }
