d6668ac00d8ecabf61bb6e1a9fc29af61c5a5f6e
Caio Severino <kaiosev9@gmail.com>
2023-03-05 19:58:40 -0300

    wip: 🚧 role middleware

---
+++ app/backend/src/middlewares/ValidateTokenMiddleware.ts
@@ -0,0 +1,26 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt = require('jsonwebtoken');
+
+const key = process.env.JWT_SECRET || 'jwt_secret';
+
+export default async function validateTokenMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  const token = req.headers.authorization;
+  if (!token) {
+    return res.status(401).json({ message: 'Token not found' });
+  }
+
+  try {
+    const decoded = await jwt.verify(token, key) as jwt.JwtPayload;
+    if (!decoded) {
+      return res.status(401).json({ message: 'Token must be valid token' });
+    }
+    req.body.token = decoded;
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: 'Token must be valid token' });
+  }
+}
--- app/backend/src/routes/UserRouter.ts
+++ app/backend/src/routes/UserRouter.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+// import validateTokenMiddleware from '../middlewares/ValidateTokenMiddleware';
 import checkCredentials from '../middlewares/LoginMiddleware';
 import UserController from '../controller/UserController';
 
@@ -7,5 +8,5 @@ const router = Router();
 const userController = new UserController();
 
 router.post('/', checkCredentials, userController.login);
-
+// router.get('/role', validateTokenMiddleware, userController.getRole);
 export default router;
