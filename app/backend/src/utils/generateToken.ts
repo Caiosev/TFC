b0fbda438816482c740c57a3897d54d50be3e4f7
Caio Severino <kaiosev9@gmail.com>
2023-03-05 14:54:37 -0300

    style: 🚨 lint rule

--- app/backend/src/utils/generateToken.ts
+++ app/backend/src/utils/generateToken.ts
@@ -2,6 +2,7 @@ import jwt = require('jsonwebtoken');
 
 const key = process.env.JWT_SECRET || 'segredo';
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default function generateToken(user: any) {
   const token = jwt.sign(user, key, {
     expiresIn: 86400,
