30bcbc6458fb4d4061572dc9c71b5b8d2c6064f3
Caio Severino <kaiosev9@gmail.com>
2023-03-12 16:18:58 -0300

    fix: 🐛 generate token exception

--- app/backend/src/utils/generateToken.ts
+++ app/backend/src/utils/generateToken.ts
@@ -3,6 +3,9 @@ import jwt = require('jsonwebtoken');
 const key = process.env.JWT_SECRET || 'jwt_secret';
 
 export default function generateToken(email: string) {
+  if (!email) {
+    throw new Error('Email must be provided');
+  }
   const token = jwt.sign({ data: { email } }, key, {
     expiresIn: '7d',
     algorithm: 'HS256',
